49c012366d71d2292b86ff0f7930c77eca4b0474
Jamal .S <me@shrjamal.com>
2021-10-23 00:22:59 +0100

    implement home page

--- layouts/default.tsx
+++ layouts/default.tsx
@@ -9,7 +9,7 @@ export default function Layout({
   return (
     <>
       <NavBar />
-      {children}
+      <main>{children}</main>
     </>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
+import { Country } from '../@types/country'
+import CountryItem from '../components/Country'
+import SearchBar from '../components/SearchBar'
+import { useCountries } from '../data/countries'
 import Layout from '../layouts/default'
 
-export default function Home() {
+type Props = {
+  countries: Country[]
+}
+
+export default function Home({ countries }: Props) {
+  const { data } = useCountries()
   return (
     <Layout>
-      <h1>Hello World</h1>
+      <div className="mx-20">
+        <SearchBar />
+        <div className="mx-auto grid gap-10 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 ">
+          {(data ?? []).map((c) => (
+            <CountryItem c={c} key={c.name + c.flag} />
+          ))}
+        </div>
+      </div>
     </Layout>
   )
 }
