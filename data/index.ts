faebb463c6ac6c26b2e1f79b727e51b9d99a5576
Jamal .S <me@shrjamal.com>
2021-10-23 00:22:09 +0100

    add axois

--- data/index.ts
+++ data/index.ts
@@ -1,3 +1,9 @@
-import { QueryClient } from 'react-query'
+import { QueryClient, QueryCache } from 'react-query'
+import Axios from 'axios'
 
 export const reactQuery = new QueryClient()
+
+export const axiosInstance = Axios.create({
+  baseURL: 'https://restcountries.com/v2',
+  timeout: 1000,
+})
