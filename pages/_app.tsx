e278e5790cc59cc7e97c9cd561c8660757927c1d
Jamal .S <me@shrjamal.com>
2021-10-23 11:36:06 +0100

    add topProgressbar

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,24 @@
 import type { AppProps } from 'next/app'
+import dynamic from 'next/dynamic'
 import { QueryClientProvider, Hydrate } from 'react-query'
 import { ReactQueryDevtools } from 'react-query/devtools'
 
 import { reactQuery } from '../data'
 import 'tailwindcss/tailwind.css'
 import '../public/main.css'
+import 'nprogress/nprogress.css'
 
+const TopProgressBar = dynamic(
+  () => {
+    return import('../components/TopProgressBar')
+  },
+  { ssr: false },
+)
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={reactQuery}>
       <Hydrate state={pageProps.dehydratedState}>
+        <TopProgressBar />
         <Component {...pageProps} />
       </Hydrate>
       <ReactQueryDevtools initialIsOpen={false} />
